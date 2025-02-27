2fb4d79eaf326c8ab850a110a74c4160a6cb1791
Fish Yu <b45s67@gmail.com>
2025-02-28 00:51:33 +0800

    deps: Update to nixpkg 24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:nixOS/nixpkgs?ref=24.05";
+        nixpkgs.url = "github:nixOS/nixpkgs?ref=branch-off-24.11";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
